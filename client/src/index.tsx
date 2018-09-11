9931d6b41bfb2f3738417fd9caad0731ed3005c7
Damien FREMONT <damien.fremont@gmail.com>
2018-09-11 22:46:11 +0200

    build: Internationalize
    defaullt language

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,11 +8,20 @@ import * as fr from 'react-intl/locale-data/fr';
 
 import App from './App';
 import * as enMessage from './i18n/translations/en.json';
+import * as frMessage from './i18n/translations/fr.json';
 import registerServiceWorker from './registerServiceWorker';
 
 /* Import basic support for another locale if needed
    ('en' is included by default) */
 addLocaleData([...en, ...fr]);
+
+const language =
+  (navigator.languages && navigator.languages[0]) ||
+  navigator.language /*||
+  navigator.userLanguage*/;
+
+// Split locales with a region code
+const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
 
 /* Define your translations */
 const i18nConfig = {
@@ -20,10 +29,24 @@ const i18nConfig = {
   messages: enMessage
 };
 
+function onChangeLanguage(lang: string) {
+  lang = lang.toLowerCase();
+  switch (lang) {
+      case 'fr': i18nConfig.messages = frMessage; break;
+      case 'en': i18nConfig.messages = enMessage; break;
+      default: i18nConfig.messages = enMessage; break;
+  }
+  // this.setState({ locale: lang });
+  i18nConfig.locale = lang;
+}
+
+// apply lang
+onChangeLanguage(languageWithoutRegionCode);
+
 ReactDOM.render(
-  <IntlProvider
-    locale="fr" defaultLocale="en-US"
-    messages={i18nConfig.messages} >
+  <IntlProvider 
+    locale={ i18nConfig.locale }  
+    messages={ i18nConfig.messages } >
     <App />
   </IntlProvider>,
   document.getElementById('root') as HTMLElement
